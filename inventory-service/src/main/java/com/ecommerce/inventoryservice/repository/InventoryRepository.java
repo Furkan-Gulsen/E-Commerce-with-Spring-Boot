b3645196a8db1a8cce1a51e4896414ebe847ba40
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2023-10-14 21:37:36 +0300

    FEATURE: Implement inventory service

---
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.inventoryservice.repository;
+
+import com.ecommerce.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+        Optional<Inventory> findBySkuCode(String skuCode);
+}
--- order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -8,12 +8,14 @@ import com.ecommerce.orderservice.model.OrderLineItems;
 import com.ecommerce.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;

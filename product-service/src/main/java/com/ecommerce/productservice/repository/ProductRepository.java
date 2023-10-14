7e575313164df959e6a4782aa2272d70f30d062f
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2023-10-14 17:51:59 +0300

    FEATURE: Product Service configurations and completed CRUD

---
+++ product-service/src/main/java/com/ecommerce/productservice/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.ecommerce.productservice.repository;
+
+import com.ecommerce.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {}
